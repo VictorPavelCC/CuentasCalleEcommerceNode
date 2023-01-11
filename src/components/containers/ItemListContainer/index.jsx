41704a9615d49265a0420ab0ae7f0534a73360f1
Victor Pavel Cuentas Calle <pavelcuentas@gmail.com>
2023-01-11 20:00:36 -0300

    fase 1

---
+++ src/components/containers/ItemListContainer/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Item from '../../Item'
+import './styles.css'
+
+
+const ItemListContainer = () => {
+  return (
+    <div className='item-list-container'>
+      <Item/>
+      <Item/>
+      <Item/>
+      <Item/>
+      
+    </div>
+  )
+}
+
+export default ItemListContainer
